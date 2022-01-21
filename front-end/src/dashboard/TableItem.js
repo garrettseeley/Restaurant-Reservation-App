154efd156b3570c14578a4c4383d97cda2a76638
Garrett <seeleysurfer01@gmail.com>
2022-01-21 13:57:02 -0600

    adjustment to tableItem.js

--- front-end/src/dashboard/TableItem.js
+++ front-end/src/dashboard/TableItem.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function TableItem({table}) {
+  let status = !table.reservation_id ? "Free" : "Occupied"
   return (
     <tr data-table-id-status={table.table_id}>
       <td>{table.table_id}</td>
       <td>{table.table_name}</td>
       <td>{table.capacity}</td>
-      <td>{!table.reservation_id ? "Free" : "Occupied"}</td>
+      <td>{status}</td>
       <td>buttons</td>
     </tr>
   )
